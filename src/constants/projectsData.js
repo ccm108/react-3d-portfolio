62fd7f5011da04dc158f11dbf2237afbfb7c7e2a
Christoper Miller <chris@mac.mynetworksettings.com>
2025-12-19 02:41:04 -0500

    Update: latest changes to projects section

--- src/constants/projectsData.js
+++ src/constants/projectsData.js
@@ -2,29 +2,29 @@ import githubLogo from "/assets/logos/github.svg";
 
 export const projects = [
   {
-    name: "Object Recognition Application",
+    name: "AI Powered Phishing Detection Tool",
     description:
-      "Developed a mobile object recognition application using TensorFlow Lite to provide real-time auditory guidance for visually impaired users.",
+      "Developed An AI-powered web app that analyzes emails and detects phishing attempts. Paste an email into the chat interface, and the AI provides a risk score, classification, and findings.",
     tags: [
-      { name: "Java", color: "text-[#33c2cc]" },
-      { name: "Python", color: "text-[#57db96]" },
-      { name: "TensorFlow", color: "text-[#57db96]" }
+      { name: "React", color: "text-[#33c2cc]" },
+      { name: "TypeScript", color: "text-[#57db96]" },
+      { name: "MachineLearning", color: "text-[#57db96]" }
     ],
     image: githubLogo,
-    source_code_link: "https://github.com/ccm108/object-recognition-app",
-    live_project_link: null,
+    source_code_link: "https://github.com/ccm108/AI-Phishing-Tool",
+    live_project_link: "https://ai-phishing-tool.vercel.app",
     gradientClass: "grid-blue-color",
   },
   {
-    name: "Work In Progress...",
+    name: "Adversary Emulation & Detection Engineering",
     description:
-      "Programming...",
+      "This project simulates several MITRE ATT&CK techniques using Atomic Red Team, and then collects every trace of activity using Sysmon. The goal is simple: run safe attacks, study the logs, and prove detection coverage.",
     tags: [
-      { name: "React", color: "text-[#33c2cc]" },
-      { name: "AWS", color: "text-[#ca2f8c]" },
+      { name: "ShellScripting", color: "text-[#33c2cc]" },
+      { name: "Cybersecurity", color: "text-[#ca2f8c]" },
     ],
     image: githubLogo,
-    source_code_link: "https://github.com/yourusername/udp-ping-client",
+    source_code_link: "https://github.com/ccm108/Adversary-Emulation-and-Detection-Engineering",
     live_project_link: null,
     gradientClass: "grid-default-color",
   },
